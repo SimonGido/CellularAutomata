34515ca1adccd75918eed5fe3121bc7db3e9731c
Šimon Gido <simongido1@gmail.com>
2021-07-21 22:42:02 +0200

    example code

--- CellularAutomata/src/GameLayer.cpp
+++ CellularAutomata/src/GameLayer.cpp
@@ -1,12 +1,97 @@
 #include "GameLayer.h"
 
+static glm::vec2 GetMouseViewportPosition(const XYZ::OrthographicCameraController& controller)
+{
+	auto [x, y] = XYZ::Input::GetMousePosition();
+	auto width = XYZ::Application::Get().GetWindow().GetWidth();
+	auto height = XYZ::Application::Get().GetWindow().GetHeight();
+
+	auto bounds = controller.GetBounds();
+	auto pos = controller.GetCamera().GetPosition();
+	x = (x / width) * bounds.GetWidth() - bounds.GetWidth() * 0.5f;
+	y = bounds.GetHeight() * 0.5f - (y / height) * bounds.GetHeight();
+	return { pos.x + x, pos.y + y };
+}
 
 GameLayer::GameLayer()
+	:
+	m_CameraController(16.0f / 9.0f),
+	m_MousePosition(0.0f)
 {
-	XYZ::Application::Get().GetImGuiLayer()->EnableDockspace(false);
+	auto& app = XYZ::Application::Get();
+	app.GetImGuiLayer()->EnableDockspace(false);
+	app.GetImGuiLayer()->BlockEvents(false);
 }
 
 GameLayer::~GameLayer()
 {
+	
+}
 
+void GameLayer::OnAttach()
+{
+	m_Texture = XYZ::Texture2D::Create(100, 100, 4, {});
+	m_Shader = XYZ::Shader::Create("Assets/Shaders/TestShader.glsl");
+
+	uint8_t* pixels = new uint8_t[100 * 100 * 4];
+	memset(pixels, 0, 100 * 100 * sizeof(uint32_t));
+	for (size_t i = 0; i < 100 * 100 * 4; i += 4)
+	{
+		pixels[i]	  = 0xff;
+		pixels[i + 3] = 0xff;
+	}
+	m_Texture->SetData(pixels, 100 * 100 * sizeof(uint32_t));
+
+	XYZ::Renderer::WaitAndRender();
+	XYZ::Renderer::BlockRenderThread();
+}
+
+void GameLayer::OnDetach()
+{
+
+}
+void GameLayer::OnUpdate(XYZ::Timestep ts)
+{
+	m_CameraController.OnUpdate(ts);
+	XYZ::Renderer::Clear();
+	XYZ::Renderer::SetClearColor({ 0.0f,0.0f,0.0f,1.0f });
+	
+	m_Shader->Bind();
+	m_Texture->Bind();
+	XYZ::Renderer::SubmitFullscreenQuad();
+
+	XYZ::Renderer2D::BeginScene(m_CameraController.GetCamera().GetViewProjectionMatrix(), m_CameraController.GetCamera().GetPosition());
+	XYZ::Renderer2D::SubmitQuad(glm::vec3(0.0f), glm::vec2(1.0f), glm::vec4(1.0f, 0.0f, 1.0f, 1.0f));
+	XYZ::Renderer2D::SubmitCircle(glm::vec3(m_MousePosition, 0.0f), 0.1f, 10);
+
+	XYZ::Renderer2D::Flush();
+	XYZ::Renderer2D::FlushLines();
+	
+	m_Timestep = ts;
+	m_RendererStats = XYZ::Renderer2D::GetStats();
+	XYZ::Renderer2D::EndScene();	
+}
+void GameLayer::OnEvent(XYZ::Event& event)
+{
+	m_CameraController.OnEvent(event);
+}
+void GameLayer::OnImGuiRender()
+{
+	if (ImGui::Begin("Debug"))
+	{
+		ImGui::Text("Performance: ");
+		ImGui::SameLine();
+		ImGui::Text("%f s", m_Timestep);
+	
+		ImGui::Text("Quad Drawcalls: ");
+		ImGui::SameLine();
+		ImGui::Text("%u", m_RendererStats.DrawCalls);
+	
+		ImGui::Text("Line Drawcalls: ");
+		ImGui::SameLine();
+		ImGui::Text("%u", m_RendererStats.LineDrawCalls);
+	
+		m_MousePosition = GetMouseViewportPosition(m_CameraController);
+	}
+	ImGui::End();
 }
--- CellularAutomata/src/GameLayer.h
+++ CellularAutomata/src/GameLayer.h
@@ -9,10 +9,19 @@ public:
 	GameLayer();
 	~GameLayer();
 
-	virtual void OnAttach() override {};
-	virtual void OnDetach() override {};
-	virtual void OnUpdate(XYZ::Timestep ts) override {};
-	virtual void OnEvent(XYZ::Event& event) override {};
-	virtual void OnImGuiRender() override {};
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnUpdate(XYZ::Timestep ts) override;
+	virtual void OnEvent(XYZ::Event& event) override;
+	virtual void OnImGuiRender() override;
+
+private:
+	XYZ::OrthographicCameraController m_CameraController;
+	XYZ::Renderer2DStats			  m_RendererStats;
+	XYZ::Timestep					  m_Timestep;
+	glm::vec2						  m_MousePosition;
+
+	XYZ::Ref<XYZ::Texture2D>		  m_Texture;
+	XYZ::Ref<XYZ::Shader>			  m_Shader;
 };
 
